6f512bcd5bd5989a7f6980ac5502223e76e1bcdb
jiangshenjie <741294595@qq.com>
2019-09-07 17:30:18 +0800

    Add MapperClosedException

--- src/main/java/com/jsj/orm/exception/MapperClosedException.java
+++ src/main/java/com/jsj/orm/exception/MapperClosedException.java
@@ -1,5 +1,8 @@
 package com.jsj.orm.exception;
 
+/**
+ * @author jiangshenjie
+ */
 public class MapperClosedException extends RuntimeException {
     public MapperClosedException(String message) {
         super(message);
