5200939aeb87678255f8f5dfb32861c711cdf0fd
jiangshenjie <741294595@qq.com>
2019-09-08 11:41:31 +0800

    增加说明

--- README.md
+++ README.md
@@ -23,5 +23,32 @@
 * 继承BaseMapper（参考示例中的UserMapper）
 * 直接调用BaseMapper方法
 
+###示例
+
+    @Test
+    public void selectName() throws Exception {
+        DataSource dataSource = getDataSource();
+        UserMapper userMapper = new UserMapperImpl(dataSource, true);
+        String userName = userMapper.selectName(1L);
+        System.out.println(userName);
+    }
+
+    @Test
+    public void selectOne() throws Exception {
+        DataSource dataSource = getDataSource();
+        UserMapper userMapper = new UserMapperImpl(dataSource, true);
+        UserDO userDO = userMapper.selectOne(1L);
+        System.out.println(userDO);
+    }
+
+    @Test
+    public void update() throws Exception {
+        DataSource dataSource = getDataSource();
+        UserMapper userMapper = new UserMapperImpl(dataSource, true);
+        userMapper.update("tom_" + System.currentTimeMillis(), 1L);
+        System.out.println("---------select-----------");
+        selectOne();
+    }
+
 
 
--- src/test/java/com/jsj/orm/JdbcDemo.java
+++ src/test/java/com/jsj/orm/JdbcDemo.java
@@ -1,6 +1,7 @@
 package com.jsj.orm;
 
 import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.jsj.orm.dao.UserMapper;
 import com.jsj.orm.dao.UserMapperImpl;
 import org.junit.Test;
 
@@ -25,14 +26,14 @@ public class JdbcDemo {
     @Test
     public void createTable() throws Exception {
         DataSource dataSource = getDataSource();
-        UserMapperImpl userMapper = new UserMapperImpl(dataSource, true);
+        UserMapper userMapper = new UserMapperImpl(dataSource, true);
         userMapper.createTableIfNotExists();
     }
 
     @Test
     public void selectName() throws Exception {
         DataSource dataSource = getDataSource();
-        UserMapperImpl userMapper = new UserMapperImpl(dataSource, true);
+        UserMapper userMapper = new UserMapperImpl(dataSource, true);
         String userName = userMapper.selectName(1L);
         System.out.println(userName);
     }
@@ -40,7 +41,7 @@ public class JdbcDemo {
     @Test
     public void selectOne() throws Exception {
         DataSource dataSource = getDataSource();
-        UserMapperImpl userMapper = new UserMapperImpl(dataSource, true);
+        UserMapper userMapper = new UserMapperImpl(dataSource, true);
         UserDO userDO = userMapper.selectOne(1L);
         System.out.println(userDO);
     }
@@ -48,7 +49,7 @@ public class JdbcDemo {
     @Test
     public void update() throws Exception {
         DataSource dataSource = getDataSource();
-        UserMapperImpl userMapper = new UserMapperImpl(dataSource, true);
+        UserMapper userMapper = new UserMapperImpl(dataSource, true);
         userMapper.update("tom_" + System.currentTimeMillis(), 1L);
         System.out.println("---------select-----------");
         selectOne();
